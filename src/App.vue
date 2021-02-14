505ba151e61ceaf82b154626bc119c129d94b497
ST-20120801\ST <ddongsa123@naver.com>
2021-02-14 18:48:31 +0900

    featue-#1/main페이지(메뉴)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>EZEN</h1>
+    <nav id ="menu">
+    <ul>
+      <li><a href="App.vue">Home</a></li>
+      <li><a href="About.vue">About</a></li>
+      <li><a href="Service.vue">Service</a></li>
+       <li><a href="Contact.vue">Contact</a></li>
+        <li><a href="Login.vue">login</a></li>
+    </ul>
+  </nav>
+ </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+   
   }
 }
 </script>
@@ -21,8 +27,39 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+}
+h1 {
+ display: flex;
+  justify-content: space-between;
+  float:left;
+  margin-top:0px;
+}
+#menu{
+  display: inline;
+  vertical-align: middle;
+  float:right;
+  padding-bottom:200px;
+}
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+}
+a {
+  display: block;
+  text-align: center;
+  margin: .25rem;
+  padding: .5rem 1rem;
+  text-decoration: none;
+  font-weight: bold;
+  color: white;
+  background: teal;
+}
+a:hover {
+  background: yellowgreen;
 }
 </style>
